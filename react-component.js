9ff7b05abaa1377a13c53356004750e3cd88d10a
crptm <crptm@users.noreply.github.com>
2017-06-23 04:21:13 +0400

    align rendering with mew V3

--- react-component.js
+++ react-component.js
@@ -6,23 +6,13 @@ import React from 'react';
 import blockies from './blockies';
 
 export default class BlockiesIdenticon extends React.Component {
-  getOpts() {
-    return {
-      seed: this.props.opts.seed || 'foo',
-      color: this.props.opts.color || '#dfe',
-      bgcolor: this.props.opts.bgcolor || '#a71',
-      size: this.props.opts.size || 15,
-      scale: this.props.opts.scale || 3,
-      spotcolor: this.props.opts.spotcolor || '#000'
-    };
-  }
-  componentDidMount() {
-    this.draw();
-  }
-  draw() {
-    blockies.render(this.getOpts(), this.canvas);
-  }
-  render() {
-    return <canvas ref={canvas => (this.canvas = canvas)} />;
-  }
+    componentDidMount() {
+        this.draw();
+    }
+    draw() {
+        blockies.render(this.props.opts || {}, this.canvas);
+    }
+    render() {
+        return <canvas ref={canvas => (this.canvas = canvas)} />;
+    }
 }
